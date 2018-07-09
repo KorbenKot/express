337f8ac6333c2f71ed29fd9d1f4fd6a5a18878bb
Alexander <rober.fmc@gmail.com>
2018-07-09 18:56:49 +0300

    [FE] magic with gulp

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const express = require('express');
-const app = express();
+const
+        express = require('express'),
+        expressHbs = require('express-handlebars'),
+        path = require('path'),
+        app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.engine('handlebars', expressHbs({
+    defaultLayout: 'main',
+    extname: '.handlebars',
+    partialsDir: path.join(__dirname, 'views/blocks'),
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', '.handlebars');
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.static('dest/css'));
+app.use(express.static('dest/fonts'));
 
 app.get('/', (req, res) => res.render('index',
     {
-        title: 'Test app',
-        message: 'Express fw',
+        title: 'Test express application',
+        message: 'Express message',
         item: [{
-            message: 'Fuck you!'
+            message: 'Item message'
         }]
     }
 ));
 
 app.get('/about', (req, res) => res.render('about'));
+app.get('/balance', (req, res) => res.render('balance'));
+app.get('/cart', (req, res) => res.render('cart'));
 
 app.listen(8080, () => console.log('My test app running on port 8080!'));
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,75 @@
-var         gulp = require ('gulp');
-var         browserSync = require ('browser-sync').create();
-var         nodemon = require ('gulp-nodemon');
-var         postcss = require ('gulp-postcss');
+const
+        gulp = require ('gulp'),
+        browserSync = require ('browser-sync').create(),
+        nodemon = require ('gulp-nodemon'),
+        postcss = require ('gulp-postcss'),
+        debug = require("gulp-debug"),
+        rename = require('gulp-rename'),
+        concat = require('gulp-concat'),
+        uglify = require('gulp-uglify'),
+        newer = require('gulp-newer');
+        fs = require('file-system');
 
 
-gulp.task('default', ['browser-sync'], function () {
-});
+gulp.task('default', ['browser-sync', 'css', 'fonts'], function () { });
 
 gulp.task('browser-sync', ['nodemon'], function () {
     browserSync.init({
         proxy: 'http://localhost:8080',
         browser: 'chrome',
-        files: ['./views/*.pug', './*.js'],
+        files: ['./views/blocks/button/*.css', './src/css/*.css', './views/blocks/*.handlebars',],
         port: 3000
     });
 
-    gulp.watch("public/scss/*.scss", ['css']);
-    gulp.watch("views/*.html").on('change', browserSync.reload);
+    gulp.watch("views/blocks/button/*.css", ['css']);
 
+    gulp.watch("views/blocks/button/*.css").on('change', function () {
+        ['css'];
+        browserSync.reload;
+    });
+    gulp.watch("scr/css/*.css", ['css']);
+    gulp.watch("views/blocks/*.handlebars").on('change', browserSync.reload);
 });
 
-
 gulp.task('css', function () {
-    var processors = [
+    let processors = [
+        require("postcss-import"),
+        require("postcss-assets")({
+            loadPaths: ['src/img/', 'src/sprite/', 'src/svg/'],
+            relative: true
+        }),
+        require('postcss-nested'),
+        require('postcss-nesting'),
+        require('postcss-simple-vars'),
+        require('postcss-custom-media'),
+        require("postcss-center"),
+        require("postcss-pxtorem"),
+        require("postcss-svg"),
+        require("postcss-inline-svg"),
+        require("postcss-short"),
+        require("cssnano")({
+            autoprefixer: false,
+            reduceIdents: false
+        })
     ];
-    return gulp.src('./src/*.css')
+
+    return gulp.src('./src/css/*')
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./dest'))
+        .pipe(concat('style.css'))
+        .pipe(rename('style.min.css'))
+        .pipe(debug({title: 'css: '}))
+        .pipe(gulp.dest('./dest/css'))
         .pipe(browserSync.stream());
+});
+
+// FONTS
+
+gulp.task('fonts', function () {
+
+    return gulp.src('./src/fonts/*')
+        .pipe(newer('./dest/fonts'))
+        .pipe(debug({title: 'fonts: '}))
+        .pipe(gulp.dest('./dest/fonts'));
 });
 
 gulp.task('nodemon', function (cb) {
@@ -37,12 +79,9 @@ gulp.task('nodemon', function (cb) {
     return nodemon({
         script: 'app.js'
     }).on('start', function () {
-        // to avoid nodemon being started multiple times
         if (!started) {
             cb();
             started = true;
         }
     });
 });
-
-
