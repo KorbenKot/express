2d0e2ceb9d13b615f44b17135cb5628efacd6cae
Alexander <rober.fmc@gmail.com>
2018-07-27 18:08:58 +0300

    [FE] added button img. done settings header

---
+++ balanceRouter.js
@@ -0,0 +1,17 @@
+const
+    express = require('express'),
+    router = express.Router();
+
+router.get('/', (req, res) => res.render('balance'));
+
+router.get('/pay', (req, res) => res.render('pay'));
+
+router.get('/pay_help', (req, res) => res.render('pay_help'));
+
+router.get('/pay_history', (req, res) => res.render('pay_history'));
+
+router.get('/pay_help', (req, res) => res.render('pay_help'));
+
+router.get('/pay_out', (req, res) => res.render('pay_out'));
+
+module.exports = router;
